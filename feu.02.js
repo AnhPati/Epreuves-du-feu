8dba1b727cd1218262a245d8fe32c611e9740ebf
Anh Pati <romain-felicianne@hotmail.fr>
2023-07-28 18:28:00 +0200

    Trouver une forme
    Récupération et affichage du contenu des fichiers.
    Mise en place de boucles pour comparer les contenu.
    Recherche sur les limites, les points d'ancrage, pour gérer les sauts de ligne.

--- feu.02.js
+++ feu.02.js
@@ -1,14 +1,49 @@
 /*Fonctions*/
-const forFind = (board) => {
+const forFind = (board, toSearch) => {
+    const boardData = dataRead(board)
+    console.log(`\x1b[031mFichier :\x1b[037m ${board}`)
+    console.log(`\x1b[035mDonnées :\x1b[037m`)
+    console.log(`${boardData}`)
+
+    const toSearchData = dataRead(toSearch)
+    console.log(`\x1b[031mFichier :\x1b[037m ${toSearch}`)
+    console.log(`\x1b[035mDonnées :\x1b[037m`)
+    console.log(`${toSearchData}`)
+
+    let boardArray = boardData.split("").filter(char => char !== "\r")
+    let toSearchArray = toSearchData.split("").filter(char => char !== "\r")
+    console.log(boardArray)
+    console.log(toSearchArray)
+    let lineCount = 0
+
+    for (let i = 0; i < toSearchArray.length; i++) {
+        // for (j = 0; j = boardArray.length; i++) {
+        //     if (toSearch[j] = boardData[i]) {
+        //         if (boardArray[j] === "\n") {
+        //             lineCount += 1
+        //             console.log(`Compteur de lignes : \x1b[036m${lineCount}\x1b[037m`)
+        //         }
+
+        //     }
+        // }
+
+        console.log(`Compteur de i : \x1b[036m${i}\x1b[037m`)
+    }
+
+
+}
+
+const dataRead = (file) => {
     const fs = require('fs')
-    
+    let data
+
     try {
-        const data = fs.readFileSync(board)
-        console.log(data)
+        data = fs.readFileSync(file, { encoding: 'utf8' })
     } catch (err) {
         console.error(err)
     }
 
+    return data
 }
 
 /*Gestion des erreurs*/
@@ -36,7 +71,7 @@ const displayForFind = () => {
         return
     }
 
-    return console.log(forFind(board))
+    return console.log(forFind(board, toSearch))
 }
 
 /*Affichage du résultat*/
