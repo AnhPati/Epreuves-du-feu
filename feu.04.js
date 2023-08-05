8547f95fe52c52ef01271e421c75c25602488772
Anh Pati <romain-felicianne@hotmail.fr>
2023-08-05 17:52:17 +0200

    Trouver le plus grand carré v3
    Récupération des instructions et du plateau.
    Création d'une fonction qui vérifie si les prochains éléments à droite, en bas et en diagonale bas sont bien des espaces libres.

--- feu.04.js
+++ feu.04.js
@@ -1,10 +1,59 @@
 /*Fonctions*/
 const findBiggestSquare = (board) => {
-    const result = dataRead(board)
+    const boardData = dataRead(board).split('')
+    const boardArray = boardData.slice(boardData.indexOf('\n') + 1)
+    const instructions = boardData.slice(0, boardData.indexOf('\n'))
+
+    const boardControllers = {
+        lineLength: Number(instructions[0]) + 1,
+        space: instructions[1],
+        obstacle: instructions[2],
+        fill: instructions[3]
+    }
+
+    const square = {
+
+    }
+
+    for (let i = 0; i < boardArray.length; i++) {
+        if (boardArray[i] === "\n") {
+            i += 1
+        }
+    }
+
+    console.log(boardControllers.lineLength)
+    console.log(boardControllers.space)
+    console.log(boardControllers.obstacle)
+    console.log(boardControllers.fill)
+    return boardArray
+}
+
+const getSquare = (boardArray, index, controllers) => {
+    let result = false
+
+    if (boardArray[index + 1] === ".") {
+        result = true
+
+        if (boardArray[index + controllers.lineLength] === ".") {
+            result = true
+
+            if (boardArray[index + controllers.lineLength + 1] === ".") {
+                result = true
+
+            } else {
+                return result
+            }
+
+        } else {
+            return result
+        }
+
+    } else {
+        return result = false
+    }
 
     return result
 }
-
 
 const dataRead = (file) => {
     const fs = require('fs')
